cf62fd3c87526a805599462146f559576a6dcc28
zzyyppqq <zzyyppqq@gmail.com>
2025-08-15 16:17:49 +0800

    /src/main/resources │  application-dev.yml // 开发环境的配置文件及入口 │  application-prod.yml // 生产环境的配置文件及入口 │  application.yml // 默认环境无关的配置文件及入口 └─conf    ├─dev    │      datasource.yml // 开发环境的数据库配置    └─prod           datasource.yml // 生产环境的数据库配置

--- src/main/kotlin/com/zyp/springboot/learn/controller/HomeController.java
+++ src/main/kotlin/com/zyp/springboot/learn/controller/HomeController.java
@@ -1,12 +1,24 @@
 package com.zyp.springboot.learn.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // 1. 加上这个注解，方法的返回值会自动序列化为JSON
 public class HomeController {
+
+    // welcome节点是我们自己加的配置，这里主要用于测试目的。我们把第一个HTTP API服务改造下，将HomeController#home方法返回的字符串改为配置文件中内容：
+    @Value("${welcome.content}") // 1. Spring会自动将配置文件中的内容注入
+    private String welcomeContent;
+
+    @GetMapping("/env")
+    public String env() {
+        return welcomeContent; // 2. 改为返回配置文件的内容
+    }
+
     @GetMapping("/") // 2. 定义这个API的url路径和可接收的Http Method（这里是GET）
     public String home() {
         return "Welcome Home";
     }
+
 }
